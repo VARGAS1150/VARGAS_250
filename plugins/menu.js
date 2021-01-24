571543a518ec3da64c1a0df8745db299a3769154
VARGAS1150 <77575442+VARGAS1150@users.noreply.github.com>
2021-01-24 14:06:44 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -60,7 +60,7 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
     }
     conn.menu = conn.menu ? conn.menu : {}
     let before = conn.menu.before || `${conn.getName(conn.user.jid)} • Bot\n\nHai, %name!\n*%exp XP*\n*%limit Limit*\n*%week %weton, %date*\n*%time*\n*Uptime: %uptime*\n*%totalreg User in database*\n\nGrup : https://chat.whatsapp.com/CWSgYJnbiFWFuQyY9VXDGb\nInfo : https://linktr.ee/VARGAS_250\n%readmore`
-    let header = conn.menu.header || '╭─「 %category 」'
+    let header = conn.menu.header || '╭════•›「 %category 」'
     let body   = conn.menu.body   || '┠ ✜► %cmd%islimit'
     let footer = conn.menu.footer || '╰═══════════════\n'
     let after  = conn.menu.after  || conn.user.jid == global.conn.user.jid ? '' : `\nPowered by https://wa.me/${global.conn.user.jid.split`@`[0]}`
