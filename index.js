fd6d3eefdfe6f1a55bcae50e0dcd479ef5f25d55
VARGAS_250 <77575442+VARGAS1150@users.noreply.github.com>
2021-01-24 18:38:05 +0700

    VARGAS_250

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ console.log('Starting...')
 let { spawn } = require('child_process')
 let path = require('path')
 const CFonts  = require('cfonts')
-CFonts.say('TERMUX WHATSAPP BOT', {
+CFonts.say('VARGAS_250 BOT', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
 })
-CFonts.say('\'wabot-aq\' By @Nurutomo', {
+CFonts.say('\'VARGAS_250 BOT\' By @VARGAS', {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
